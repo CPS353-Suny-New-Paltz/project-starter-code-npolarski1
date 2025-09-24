f1fbfee01059230b240b0f555eb181c3be99253c
Nathan Polarski <nathaniel.polarski@ouboces.org>
2025-09-24 17:04:55 -0400

    Added empty API implementations

---
+++ src/ComputeComponentAPI.java
@@ -0,0 +1,15 @@
+import shared.ComputationResult;
+import shared.InputInts;
+
+public class ComputeComponentAPI implements conceptualapi.ComputeComponentAPI {
+
+	public ComputationResult compute(InputInts inputInts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void saveResult(ComputationResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+}
