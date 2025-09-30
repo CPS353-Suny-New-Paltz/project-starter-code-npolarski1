71bdaa4bafd5ad839cf3da70905f14c09773e83e
Nathan Polarski <nathaniel.polarski@ouboces.org>
2025-09-30 15:34:38 -0400

    Implemented data storage component
    Only reads and writes to files right now for testing

--- src/implementapi/ComputeDataStorageAPI.java
+++ src/implementapi/ComputeDataStorageAPI.java
@@ -1,38 +1,59 @@
 package implementapi;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import shared.ComputationResult;
+import shared.Delimiter;
 import shared.InputInts;
 import shared.InputSource;
 import shared.OutputSource;
 import shared.ProcessResponse;
-import shared.Status;
 
 public class ComputeDataStorageAPI implements processapi.ComputeDataStorageAPI {
 	
 	private InputSource inputSource;
 	private OutputSource outputSource;
+	private Delimiter delim;
 
 	@Override
 	public InputInts readInput() {
-		// TODO Auto-generated method stub
-		return null;
+		// temporary implementation for testing purposes
+		String filePath = "src/input.txt";
+		
+		List<Integer> inputList = new ArrayList<Integer>();
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            delim = new Delimiter(reader.readLine());
+            String[] inputStrings = reader.readLine().split(delim.getDelim());
+            for (String inputString : inputStrings) {
+            	inputList.add(Integer.parseInt(inputString));
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+		
+		return new InputInts(inputList);
 	}
 
 	@Override
-	public ComputationResult processInputInts(InputInts inputInts) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Status writeOutput(ComputationResult compResult) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public InputSource getInputSource() {
-		// TODO Auto-generated method stub
-		return null;
+	public ProcessResponse writeOutput(ComputationResult compResult) {
+		// temporary input for testing purposes
+		String filePath = "src/output.txt";
+		
+		try (FileWriter writer = new FileWriter(filePath)) {
+			for (Integer i : compResult.getPrimeList()) {
+				writer.write(i);
+			}
+			writer.write("\n");
+		} catch (IOException e) {
+            return ProcessResponse.FAIL;
+        }
+		
+		return ProcessResponse.SUCCESS;
 	}
 
 	public ProcessResponse setInputSource(InputSource inputSource) {
