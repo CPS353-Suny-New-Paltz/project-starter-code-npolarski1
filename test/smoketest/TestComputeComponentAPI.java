672c63e51dc1714acf4cd3e7470159d592eb5c47
Nathan Polarski <nathaniel.polarski@ouboces.org>
2025-10-02 12:25:43 -0400

    Added extra edge case smoke test

--- test/smoketest/TestComputeComponentAPI.java
+++ test/smoketest/TestComputeComponentAPI.java
@@ -25,4 +25,18 @@ public class TestComputeComponentAPI {
 		correctOutput.add(5);
 		assert results.get(0).getPrimeList().equals(correctOutput);
 	}
+
+	@Test
+	public void testComputeWithValuesLessThanTwo() {
+		List<Integer> inputInts = new ArrayList<Integer>();
+		inputInts.add(0);
+		inputInts.add(1);
+		ComputeComponentAPI testApi = new ComputeComponentAPI();
+		InputInts wrappedInput = new InputInts(inputInts);
+		testApi.setInput(wrappedInput);
+		List<ComputationResult> results = testApi.compute();
+		assert results.size() == 2;
+		assert results.get(0).getPrimeList().isEmpty();
+		assert results.get(1).getPrimeList().isEmpty();
+	}
 }
