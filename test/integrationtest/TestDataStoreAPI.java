1b75579ea10ac76213811025a310eecce90414f5
Nathan Polarski <nathaniel.polarski@ouboces.org>
2025-10-02 12:59:57 -0400

    Fixed bugs so checkpoint 3 tests pass again

--- test/integrationtest/TestDataStoreAPI.java
+++ test/integrationtest/TestDataStoreAPI.java
@@ -3,11 +3,14 @@ package integrationtest;
 import java.util.ArrayList;
 import java.util.List;
 
+import processapi.ComputeDataStorageAPI;
 import shared.ComputationResult;
 import shared.InputInts;
+import shared.InputSource;
+import shared.OutputSource;
 import shared.ProcessResponse;
 
-public class TestDataStoreAPI {
+public class TestDataStoreAPI implements ComputeDataStorageAPI {
 	
 	private TestInputSource inputSource;
 
@@ -43,6 +46,25 @@ public class TestDataStoreAPI {
 		}
 		current.add(line.toString());
 		outputSource.setOutput(current);
+		return ProcessResponse.SUCCESS;
+	}
+
+	// Interface-compatible setters that adapt shared wrappers to test doubles
+	@Override
+	public ProcessResponse setInputSource(InputSource inputSource) {
+		if (inputSource == null || !(inputSource.getInputSource() instanceof TestInputSource)) {
+			return ProcessResponse.FAIL;
+		}
+		this.inputSource = (TestInputSource) inputSource.getInputSource();
+		return ProcessResponse.SUCCESS;
+	}
+
+	@Override
+	public ProcessResponse setOutputSource(OutputSource outputSource) {
+		if (outputSource == null || !(outputSource.getOutputSource() instanceof TestOutputSource)) {
+			return ProcessResponse.FAIL;
+		}
+		this.outputSource = (TestOutputSource) outputSource.getOutputSource();
 		return ProcessResponse.SUCCESS;
 	}
 
