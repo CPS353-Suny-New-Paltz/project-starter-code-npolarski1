6eb675ecc86decfa0b30f5a1d02a4c3a2860fad9
Nathan Polarski <nathaniel.polarski@ouboces.org>
2025-10-25 11:48:30 -0400

    Fixed checkstyle bug

--- test/integrationtest/ComputeComponentAPIExceptionHandlingTest.java
+++ test/integrationtest/ComputeComponentAPIExceptionHandlingTest.java
@@ -1,7 +1,8 @@
 package integrationtest;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import implementapi.ComputeComponentAPI;
 import java.util.List;
