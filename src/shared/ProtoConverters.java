9bc9ffd299e60b20247e33e7bd442f44cba202a4
Nathan Polarski <nathaniel.polarski@ouboces.org>
2025-12-04 20:15:15 -0500

    Update proto convertors for Input/OutputSource

--- src/shared/ProtoConverters.java
+++ src/shared/ProtoConverters.java
@@ -4,30 +4,52 @@ import networkapi.grpc.InputSource;
 import networkapi.grpc.OutputSource;
 import networkapi.grpc.ProcessResponse;
 import networkapi.grpc.Delimiter;
-import com.google.protobuf.Any;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProtoConverters {
 
     public static shared.InputSource fromProto(InputSource proto) {
-        shared.InputSource in = new shared.InputSource(proto.getInputSource());
-        return in;
+        if (proto == null) return new shared.InputSource();
+        // If manual ints are present, prefer them
+        if (proto.getManualIntsCount() > 0) {
+            List<Integer> ints = new ArrayList<Integer>(proto.getManualIntsList());
+            return new shared.InputSource(ints);
+        }
+        // Otherwise use file path if present
+        if (proto.hasFilePath()) {
+            return new shared.InputSource(proto.getFilePath());
+        }
+        return new shared.InputSource();
     }
 
     public static InputSource toProto(shared.InputSource in) {
-        return InputSource.newBuilder()
-        		.setInputSource((Any) in.getInputSource())
-        		.build();
+        InputSource.Builder b = InputSource.newBuilder();
+        if (in == null) return b.build();
+        if (in.getManualInts() != null && !in.getManualInts().isEmpty()) {
+            b.addAllManualInts(in.getManualInts());
+        } else if (in.getFilePath() != null) {
+            b.setFilePath(in.getFilePath());
+        }
+        return b.build();
     }
 
     public static shared.OutputSource fromProto(OutputSource proto) {
-        shared.OutputSource out = new shared.OutputSource(proto.getOutputSource());
-        return out;
+        if (proto == null) return new shared.OutputSource();
+        if (proto.hasFilePath()) {
+            return new shared.OutputSource(proto.getFilePath());
+        }
+        return new shared.OutputSource();
     }
 
     public static OutputSource toProto(shared.OutputSource out) {
-        return OutputSource.newBuilder()
-        		.setOutputSource((Any) out.getOutputSource())
-        		.build();
+        OutputSource.Builder b = OutputSource.newBuilder();
+        if (out == null) return b.build();
+        if (out.getFilePath() != null) {
+            b.setFilePath(out.getFilePath());
+        }
+        return b.build();
     }
 
     public static shared.ProcessResponse fromProto(ProcessResponse proto) {
@@ -36,8 +58,8 @@ public class ProtoConverters {
 
     public static ProcessResponse toProto(shared.ProcessResponse resp) {
         return ProcessResponse.newBuilder()
-        		.setIsSuccess(resp.isSuccess())
-        		.build();
+                .setIsSuccess(resp.isSuccess())
+                .build();
     }
 
     public static shared.Delimiter fromProto(Delimiter proto) {
@@ -47,7 +69,7 @@ public class ProtoConverters {
 
     public static Delimiter toProto(Delimiter delim) {
         return Delimiter.newBuilder()
-        		.setDelim(delim.getDelim())
-        		.build();
+                .setDelim(delim.getDelim())
+                .build();
     }
 }
